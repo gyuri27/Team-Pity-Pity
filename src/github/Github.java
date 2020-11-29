97079bb6f3b5059af04078e424d8c31ccb8dcc16
Soltész M. Bence <Soltsz M. Bence@MisiPC>
2020-11-29 16:16:44 +0100

    hozzáadtam 1 sort

--- src/github/Github.java
+++ src/github/Github.java
@@ -15,7 +15,8 @@ public class Github {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Faszomat az egészbe!");
+        System.out.println("Azt az egészbe!");
+        System.out.print("ANYÁÁÁÁÁÁÁÁÁÁÁMMMMMMMM végre!");
     }
     
 }
